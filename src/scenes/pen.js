c436a8829f13fecfbf56f715bf02f00a10b0d6f0
Diego Diaz <berith.py@gmail.com>
2019-01-26 16:59:08 -0300

    Dialog system working!

--- src/scenes/pen.js
+++ src/scenes/pen.js
@@ -8,12 +8,12 @@ export default class Pen extends Phaser.Scene {
 
   create() {
     const mainDialog = new Dialog(this);
-    mainDialog.printLetter('Hola soy el dialog box');
+    mainDialog.start([{text:'buenas tardes'},{text:'como estas?'}]);
     this.creature = new Creature(this, 100, 100, 'ecksdee');
     this.add.existing(this.creature);
   }
 
   update() {
-
+    
   }
 }
