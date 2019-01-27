63a424b8d580190c2c783f767d056c931ea0b346
Alejo Carballude <alejocarballude@gmail.com>
2019-01-27 16:55:18 -0300

    Creature movement system.

--- src/actors/creatures/creature.js
+++ src/actors/creatures/creature.js
@@ -9,25 +9,97 @@ export const State = Enum(
 
 export default class Creature extends Actor {
   constructor (scene, x, y, asset=0xffffff, 
-               energy=100, energy_variance=20, 
-               speed=200, speed_variance=50) {
+               energyBase=50, energyVariance=25, 
+               movementBase=80, movementVariance=30,
+               speedBase=80, speedVariance=20) {
     super(scene, x, y, asset);
-    
-    this.pos_angle = 0;
-    this.energy = energy;
-    this.energy_variance = energy_variance;
-    this.speed = speed;
-    this.speed_variance = speed_variance;
 
     // TODO Remove this!
     this.setTintFill(asset)
+    
+    // Creature properties
+    //  Energy
+    this.energyBase = energyBase;
+    this.energyVariance = energyVariance;
+    //  Movement
+    this.movementBase = movementBase;
+    this.movementVariance = movementVariance;
+    //  Speed
+    this.speedBase = speedBase;
+    this.speedVariance = speedVariance;
+
+    // State
+    //  State times
+    this.idleTime = 100;
+    this.moveTime = 100;
+    //  Set initial state
+    this.setStateIdle();
 
     scene.add.existing(this);
+  }
+
+  calcVariance(base, variance) {
+    return base + (-1 + Math.random() * 2) * variance;
+  }
+
+  setEnergyRate() {
+    this.energyRate = this.calcVariance(this.energyBase, this.energyVariance);
+  }
+
+  setSpeedRate() {               
+    this.speedRate = this.calcVariance(this.speedBase, this.speedVariance);
+  }
+
+  setStateIdle() {
+    this.state = State.IDLE;
+    this.energy = 0;
+    this.setEnergyRate();
+  }
+
+  setStateMove() {
+    this.state = State.MOVING;
+    this.setSpeedRate();
+    this.moveStartTween();
+  }
+
+  moveCalcDuration() {
+    return this.moveTime * this.moveTime / this.speedRate;
+  }
+
+  moveStartTween() {
+    const angle = Math.random() * Math.PI * 2;
+    const distance = this.calcVariance(this.movementBase, this.movementVariance);
+    const deltaX = Math.cos(angle) * distance;
+    const deltaY = Math.sin(angle) * distance;
+
+    let x = this.x + deltaX;
+    if (x - this.width / 2 < 0) x -= 2 * deltaX;
+    else if (x + this.width / 2 > this.scene.width) x -= 2 * deltaX;
+
+    let y = this.y + deltaY;
+    if (y - this.height / 2 < 0) y -= 2 * deltaY;
+    else if (y + this.height / 2 > this.scene.height) y -= 2 * deltaY;
+
+    console.log('Moving to', x, ',', y);
+
+    this.scene.add.tween({ 
+      targets: this,
+      x: x,
+      y: y,
+      ease: 'EaseIn',
+      duration: this.moveCalcDuration(),
+      onComplete: this.setStateIdle.bind(this),
+    });
   }
 
   preUpdate(time, delta) {
     super.preUpdate(time, delta);
 
-
+    if (this.state == State.IDLE && this.energy < 100) {
+      this.energy += this.energyRate * delta / 1000;
+    }
+    else if (this.state == State.IDLE && this.energy >= 100) {
+      this.setStateMove()
+    }
   }
 }
--- src/actors/dialog.js
+++ src/actors/dialog.js
@@ -1,73 +1,47 @@
 export default class Dialog {
-  constructor(scene, rx=500, ry=500) {
-    this.scene = scene;
-
-    this.dialogBox = this.scene.add.rectangle(rx, ry, 700, 100, 0xff);
-    this.textObject = this.scene.make.text({
-      x: 280,
-      y: 670,
+  constructor(scene) {
+    this.dialogBox = scene.add.rectangle(500, 500, 700, 100, 0xff);
+    this.textObject = scene.make.text({
+      x: 180,
+      y: 470,
       text: '',
+      // origin: { x: 0.5, y: 0.5 },
       style: {
         font: 'bold 14px Arial',
         fill: 'white',
-        wordWrap: {width: 640, useAdvancedWrap: true},
-      },
+        wordWrap: { width: 640, useAdvancedWrap: true }
+      }
     });
-    this.dialogBox.alpha = 0;
-    this.textObject.alpha = 0;
-    this.wordDelay = 20;
-    this.phraseDelay = 2000;
-    const keys = this.scene.input.keyboard.addKey('SPACE');
-    this.scene.input.keyboard.on('keyup_SPACE', () => {
-      this.skip = true;
-    });
+    this.delay = 20
   }
 
-  write(conversation) {
+  start(conversation) {
     this.conversation = conversation || this.conversation;
-    if (Array.isArray(this.conversation) && this.conversation.length) {
-      // make box Appear
-      this.appear = true;
-
-      const interaction = this.conversation.shift();
-      const delay = interaction.delay ? interaction.delay : this.wordDelay;
-      this.textObject.text = '';
-      this.printLetter(interaction.text, delay);
-    } else {
+    if(Array.isArray(this.conversation) || Array.length){
+      let interaction = this.conversation.shift();
+      let delay = interaction.delay ? interaction.delay : this.delay
+      this.textObject.text=''
+      this.printLetter(interaction.text,delay);
     }
   }
-  printLetter(text, delay = this.wordDelay, index = 0) {
+  printLetter(text, delay = this.delay, index = 0) {
     this.textObject.text += text[index];
-    let newIndex = index + 1;
+    const newIndex = index + 1;
     if (this.skip) {
-      this.skip = false;
+      this.skip != this.skip;
       this.textObject.text = text;
-      newIndex = text.length;
     }
     if (text.length == newIndex) {
-      setTimeout(() => {
-        this.write();
-      }, this.phraseDelay);
+      setTimeout(()=>{this.start()},1000);
     }
     if (newIndex < text.length) {
       setTimeout(() => {
-        this.printLetter(text, delay, newIndex);
+        this.printLetter(text, delay,newIndex);
       }, delay);
     }
   }
+
   update() {
-    if (this.appear && this.textObject.alpha <= 1) {
-      this.dialogBox.alpha += 0.1;
-      this.textObject.alpha += 0.1;
-    } else if (!this.appear && this.textObject.alpha >= 0) {
-      this.dialogBox.alpha -= 0.1;
-      this.textObject.alpha -= 0.1;
-    }
-  }
-  disappear() {
-    // make box disappear
-    this.appear = false;
-    // clear
-    this.textObject.text = '';
+
   }
 }
